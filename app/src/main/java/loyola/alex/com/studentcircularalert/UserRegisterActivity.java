a2251c9875652bc3cbee9bad293899b1e13d4bca
jojus <itsjustinjoy@gmail.com>
2017-10-20 02:46:20 +0530

    drawable

--- app/src/main/java/loyola/alex/com/studentcircularalert/UserRegisterActivity.java
+++ app/src/main/java/loyola/alex/com/studentcircularalert/UserRegisterActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -48,6 +49,10 @@ public class UserRegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_register);
+
+        Toolbar tool = (Toolbar) findViewById(R.id.toolbar);
+        tool.setTitle("Signup");
+        setSupportActionBar(tool);
         /*DATABASE REFERENCE*/
         mDatabaseReference = FirebaseDatabase.getInstance().getReference("users");
 
